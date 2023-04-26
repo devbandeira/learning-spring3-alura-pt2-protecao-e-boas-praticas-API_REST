6a47f096a7f4385ed39c7a3a52d9cf2b3a612a03
mempet <mempet2022@gmail.com>
2023-04-26 16:01:38 -0300

    Padronizando retornos da API - ResponseEntity

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -24,6 +24,10 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        /*O cadastro vai ser um pouco diferente pq existe um codigo 201 "Requisicao processada e novo recurso criado
+        * . E esse codigo tem um tratamento especial. E vamos utilizar aqui tbm o que foi criado par ao atualizar
+        * o DTO "DadosDetalhamentoMedico" 
+        * */
         var medico = new Medico(dados);
         repository.save(medico);
 
@@ -33,9 +37,11 @@ public class MedicoController {
     }
 
     @GetMapping
+    /*Envolto o page<DadosListagemMedico> passando como GENERiCS e retornando um RESPONSEENTITY*/
     public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        /*troco o retorno porque ele nao vai retornar diretamente a variavel PAGE*/
         var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
-        return ResponseEntity.ok(page);
+        return ResponseEntity.ok(page);/*Devolvendo o codigo 200 passando o codigo PAGE(paginacao) com os dados dos medicos*/
     }
 
     @PutMapping
@@ -43,8 +49,14 @@ public class MedicoController {
     public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+/*devolvendo os dados medicos que foram atualizados*/
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));/*Dentro dos () tenho que passar o objeto MEDICO, mas nao posso passar ele direto.
+        PQ esse objeto medico e uma ENTIDADE JPA e como premissa "NAO E RECOMENDADO DEVOLVER E RECEBER ENTEIDADES JPA NO CONTROLLER
 
-        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+        ENTAO VOU DEVOLVER UM DTO, MAS O DTO QUE TENHO "DADOSATUALIZACAOmeDICO" ele e incompleto, ele representa os dados da atualizacao do medico, o que vai ser enviado para att aas informacoes.
+        Como quero devolver todos os dados atualizados do MEDICO, vou criar um outro DTO que quero devolver os dados "DadosDetalhamentoMedico".
+
+        */
     }
 
     @DeleteMapping("/{id}")
@@ -52,8 +64,11 @@ public class MedicoController {
     public ResponseEntity excluir(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
-
-        return ResponseEntity.noContent().build();
+/*Para fazer o retorno adequado tiro o VOID eu utilizo uma CLASSE do SPRING "RESPONSEENTITY" para retornar um OBJETO do tipo RESPONSEENTITY e
+* adicionar o retorno INSTANCIANDO O OBJETO RESPONSEENTITY . Utilizando seus metodos estaticos.*/
+        /*Se eu por somente ate o noContent(). ele da um erro de compilacao porque o noContent() nao retorna um objeto RESPONSENEITY, entao uso o BUILD.
+        * noContent() cria o objeto e chamo o BUILD para ai sim construir o objeto.*/
+        return ResponseEntity.noContent().build();/*Respondo "Codigo 204" dizendo que a requisicao foi processada e sem um corpo/conteudo*/
     }
 
     @GetMapping("/{id}")
--- src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
@@ -2,9 +2,15 @@ package med.voll.api.domain.medico;
 
 import med.voll.api.domain.endereco.Endereco;
 
+/*O recordo vai ser CRIADO automaticamente recebendo um OBJETO MEDICO, mas nao quero isso. Entao vou passar as informacoes do medico "long id ......."*/
+
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
 
+    /*Como la no MedicoCOntroller no metodo ATUALIZAR eu to passando um objeto MEDICO, eu posso criar um CONSTRUTOR AQUI
+    * O construtor aqui recebe um OBJETO do tipo medico e ele chama o construtor principal "THIS" do nosso RECORD passando os parametros.
+    * */
     public DadosDetalhamentoMedico(Medico medico) {
         this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+    //o THIS aqui diz que to chamando o proprio contrutor do meu RECORD. CTRL + CLICK ele leva ate ele.
     }
 }
