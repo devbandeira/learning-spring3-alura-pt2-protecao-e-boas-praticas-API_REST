68111d1c5b6c1e54033aa4bafb50dc4934dc9fdd
mempet <mempet2022@gmail.com>
2023-05-07 11:59:38 -0300

    Tratando erro 400 - CRIANDO DTO PARA RETORNAR O ERRO PERSONALIZADO

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -15,15 +15,30 @@ public class TratadorDeErros {
         return ResponseEntity.notFound().build();
     }//.notFound().build() e para criar o erro 404 "notFound() e retornar ele 404.
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    /*Personalizando o ERRO 400, para nao retornar uma array dificil no retorno do erro padrao SPRING*/
+    @ExceptionHandler(MethodArgumentNotValidException.class)/*Nome da exception que acontece pelo BEAN VALIDATION MethodArgumentNotValidException (que e o erro tratado nos parametros por nos @NotNull, @NotBlank, etc.*/
     public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
-        var erros = ex.getFieldErrors();
+        /*Diferente do erro 404, aqui eu preciso capturar a EXCEPTION que foi lancada, para saber quais campos que estao invalidos de acordo com o bean validation. ENTAO NA SSINATURA DO METODO EU POSSO RECEBER A EXCEPTION QUE FOI LANCADA.
+        * MethodArgumentNotValidException ex ("botando a exceptiona na assitura do metodo para receber a EXCEPTION")*/
+        var erros = ex.getFieldErrors();/*Jogando em uma variavel para PEGAR OS ERROS QUE ACONTECERAM, RETORNARA ISSO EM UMA LISTA DE ERROS EM CADA UM DOS CAMPOS*/
+        /*PARA TESTE vou passar o erros para retornar direto e ver o que me retorna return ResponseEntity.badRequest().body(erros) nesse caso n preciso usar o .build() ja que passo o body. Mas vi que ele retorna aquele mesmo JSON que tem todas as informacoes que vim aqui tratar*/
+        /*Entao para retornar somente o que eu quero da Exception lancad,a eu vou ter que criar um DTO. Como vou usar
+        * esse DTO somente aqui dentro, vou criar ele aqui mesmo e vou chamar de DadosErroValidacao*/
+        /*Entao no body eu nao vou passar uma lista de "erros", eu vou ter que converter esse lista para uma lista de DadosErroValidacao*/
+
+        /*******************************************************************/
+        /*Chamando os recursos do JAVA 8 para fazer a conversao de uma lista para outra "erros.stream().map()": erros me da uma stream e mapeia cada objetio FieldErros para um objeto DadosErroValidacao::new(::new) para chamar o construtor e .toList() para converser para uma lista */
+        /*So dessa forma citada a cima dara erro, porque ainda nao tenho um construtor dentro do meu DTO um objeto Field.
+        * Entao dentro do meu RECORD eu vou criar um construtor que recebe o OBJETO fieldError  */
+
+        /*se eu deixar return ResponseEntity.badRequest().build() nao me retorna um erro na resposta, somente o codigo do erro 400 bad request. Faltou o corpo da resposta dizendo qual foi o dado invalido o que dificulta o usuario da nossa api*/
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+        /**/
     }
 
     private record DadosErroValidacao(String campo, String mensagem) {
         public DadosErroValidacao(FieldError erro) {
-            this(erro.getField(), erro.getDefaultMessage());
+            this(erro.getField(), erro.getDefaultMessage());/*erro.getFiel() me da o nome do campo e getDefaultMessage() me da a mensagem para aqueel campo especifico*/
         }
     }
 
